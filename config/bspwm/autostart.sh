23d4e4100a5b03b12a115931a540d8e107b5f822
dimsp98 <125738354+dimsp98@users.noreply.github.com>
2024-03-25 12:11:45 +0200

    Update autostart.sh
    Signed-off-by: dimsp98 <125738354+dimsp98@users.noreply.github.com>

--- config/bspwm/autostart.sh
+++ config/bspwm/autostart.sh
@@ -1,8 +1,7 @@
 #!/bin/zsh
 
 #Xorg management
-xrandr --output HDMI-1-2 --primary --mode 1920x1080 --primary --rate 120 --dpi 60 --rotate normal & 
-xrandr --output eDP-1-1 --off &
+xrandr --output HDMI-1-2 --primary --mode 1920x1080 --primary --rate 120 --dpi 60 --rotate normal --output eDP-1-1 --off & 
  
 #compositor
 pgrep -x picom > /dev/null || picom -b --config ~/.config/picom/picom.conf &
