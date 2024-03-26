079f0824c22bb3b4099d94aa30acd886a2ff693f
dimsp98 <dspiliotis98@gmail.com>
2024-03-26 23:41:15 +0200

    added polybar and alacritty build commands

--- setup-scripts/mint.sh
+++ setup-scripts/mint.sh
@@ -16,6 +16,38 @@ sudo ln -sf $HOME/dotfiles/icons/* /usr/share/icons
 sudo ln -sf $HOME/dotfiles/config/lf-ueberzug/* /usr/local/bin
 sudo ln -sf $HOME/dotfiles/themes/* /usr/share/themes
 
+#alacritty
+echo "Installing dependencies"
+sudo apt install cmake pkg-config libfreetype6-dev libfontconfig1-dev libxcb-xfixes0-dev libxkbcommon-dev python3
+git clone https://github.com/alacritty/alacritty.git
+cd alacritty
+echo "building"
+cargo build --release
+infocmp alacritty
+sudo tic -xe alacritty,alacritty-direct extra/alacritty.info
+sudo cp target/release/alacritty /usr/local/bin # or anywhere else in $PATH
+sudo cp extra/logo/alacritty-term.svg /usr/share/pixmaps/Alacritty.svg
+sudo desktop-file-install extra/linux/Alacritty.desktop
+sudo update-desktop-database
+cd ~
+
+#polybar
+echo "Installing Dependencies"
+sudo apt install build-essential git cmake cmake-data pkg-config python3-sphinx python3-packaging libuv1-dev libcairo2-dev libxcb1-dev libxcb-util0-dev libxcb-randr0-dev libxcb-composite0-dev python3-xcbgen xcb-proto libxcb-image0-dev libxcb-ewmh-dev libxcb-icccm4-dev
+sudo apt install libxcb-xkb-dev libxcb-xrm-dev libxcb-cursor-dev libasound2-dev libpulse-dev i3-wm libjsoncpp-dev libmpdclient-dev libcurl4-openssl-dev libnl-genl-3-dev
+echo "optional dependencies"
+sudo apt install libxcb-xkb-dev libxcb-xrm-dev libxcb-cursor-dev libasound2-dev libpulse-dev i3-wm libjsoncpp-dev libmpdclient-dev libcurl4-openssl-dev libnl-genl-3-dev
+echo "cloning source code"
+git clone --recursive https://github.com/polybar/polybar
+cd polybar
+echo "building polybar"
+mkdir build
+cd build
+cmake ..
+make -j$(nproc)
+sudo make install
+cd ~
+
 #Change theme for bat
 bat cache --build
 
