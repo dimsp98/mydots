8371a5b91f18a7c0aedb3cc7f7927a4c7302832b
dimsp98 <125738354+dimsp98@users.noreply.github.com>
2024-04-07 10:40:47 +0200

    Update void.sh
    Signed-off-by: dimsp98 <125738354+dimsp98@users.noreply.github.com>

--- setup-scripts/void.sh
+++ setup-scripts/void.sh
@@ -2,7 +2,7 @@
 
 #Install Dependencies
 cd ~ && echo "Installing Dependencies"
-sudo xbps-install arandr bluez kitty nemo elogind rofi picom yt-dlp syncthing ImageMagick void-repo-nonfree void-repo-multilib void-repo-multilib-nonfree nodejs pnpm xorg xf86-video-nouveau base-devel git bat papirus-icon-theme papirus-folders lf ueberzug ffmpeg ghostscript zip unzip gzip lxappearance libX11-devel libXinerama-devel libXft-devel xsetroot curl openssh wget bspwm htop pulseaudio pamixer pavucontrol alsa-plugins-pulseaudio openjdk-jre mpd ncmpcpp sxhkd xclip dunst mpv maim libreoffice pfetch firefox sddm qt5-graphicaleffects qt5-quickcontrols2 zsh neovim polybar jsoncpp feh zathura-pdf-mupdf hplip cups simple-scan ntfs-3g udisks2
+sudo xbps-install arandr bluez rustup pnpm kitty nemo elogind rofi picom yt-dlp syncthing ImageMagick void-repo-nonfree void-repo-multilib void-repo-multilib-nonfree nodejs pnpm xorg xf86-video-nouveau base-devel git bat papirus-icon-theme papirus-folders lf ueberzug ffmpeg ghostscript zip unzip gzip lxappearance libX11-devel libXinerama-devel libXft-devel xsetroot curl openssh wget bspwm htop pipewire pamixer pavucontrol openjdk-jre mpd ncmpcpp sxhkd xclip dunst mpv maim libreoffice pfetch firefox sddm qt5-graphicaleffects qt5-quickcontrols2 zsh neovim polybar jsoncpp feh zathura-pdf-mupdf hplip cups simple-scan ntfs-3g udisks2
 
 #creating directories
 echo "Creating directories"
@@ -39,7 +39,8 @@ bat cache --build
 chsh -s /usr/bin/zsh && sudo chsh -s /usr/bin/zsh
 
 #NVim
-sudo xbps-install tree-sitter lazygit ripgrep sqlite fd yarn lldb nvm && cd ~
+rustup toolchain install nightly
+sudo xbps-install tree-sitter zoxide lazygit ripgrep sqlite fd yarn lldb nvm && cd ~
 
 #Configure fonts
 sudo ln -s /usr/share/fontconfig/conf.avail/70-no-bitmaps.conf /etc/fonts/conf.d/
