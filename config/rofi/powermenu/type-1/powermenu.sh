d07c42c35c53c7b95b93ed49c64ec80472a91613
dimsp98 <125738354+dimsp98@users.noreply.github.com>
2024-07-01 13:32:14 +0300

    Update powermenu.sh
    Signed-off-by: dimsp98 <125738354+dimsp98@users.noreply.github.com>

--- config/rofi/powermenu/type-1/powermenu.sh
+++ config/rofi/powermenu/type-1/powermenu.sh
@@ -62,13 +62,13 @@ run_cmd() {
 	selected="$(confirm_exit)"
 	if [[ "$selected" == "$yes" ]]; then
 		if [[ $1 == '--shutdown' ]]; then
-			systemctl poweroff
+			loginctl poweroff
 		elif [[ $1 == '--reboot' ]]; then
-			systemctl reboot
+			loginctl reboot
 		elif [[ $1 == '--suspend' ]]; then
 			mpc -q pause
 			amixer set Master mute
-			systemctl suspend
+			loginctl suspend
 		elif [[ $1 == '--logout' ]]; then
 			if [[ "$DESKTOP_SESSION" == 'openbox' ]]; then
 				openbox --exit
