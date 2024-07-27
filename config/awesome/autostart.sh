4e51972bb8f84fd329cc80d3e9dd55fd1f83f514
dimsp98 <dspiliotis98@gmail.com>
2024-07-27 21:21:41 +0200

    updated

--- config/awesome/autostart.sh
+++ config/awesome/autostart.sh
@@ -29,5 +29,5 @@ pgrep -x dunst >/dev/null || dunst -c ~/.config/dunst/dunstrc &
 nm-applet &
 
 #audio
+pipewire &
 pgrep -x volumeicon >/dev/null || volumeicon &
-pgrep -x pipewire >/dev/null || pipewire &
