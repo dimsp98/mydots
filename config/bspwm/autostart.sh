722ef9c03097c60c7f75d0ac0199d239340e2e85
dimsp98 <dspiliotis98@gmail.com>
2024-08-03 02:48:27 +0300

    updated

--- config/bspwm/autostart.sh
+++ config/bspwm/autostart.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 #Xorg management
-#xrandr --output HDMI-2 --primary --mode 1920x1080 --rate 120 --dpi 60 --rotate normal --output eDP-1 --off &
-xrandr --output eDP-1 --primary --mode 1920x1080 --rate 60 --dpi 60 --rotate normal &
+xrandr --output HDMI-2 --primary --mode 1920x1080 --rate 120 --dpi 60 --rotate normal --output eDP-1 --off &
+xrandr --output eDP-1 --mode 1920x1080 --rate 60 --dpi 60 --rotate normal &
 
 #cursor
 xsetroot -cursor_name left_ptr &
--- config/bspwm/bar.sh
+++ config/bspwm/bar.sh
@@ -6,7 +6,12 @@ pkill polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-# Launch polybar 
-polybar -c $HOME/.config/polybar/config.ini &
+if type "xrandr"; then
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    MONITOR=$m polybar -c $HOME/.config/polybar/config.ini &
+  done
+else
+  polybar -c $HOME/.config/polybar/config.ini &
+fi
 
 echo "Polybar launched"
