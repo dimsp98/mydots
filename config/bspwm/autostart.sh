3963bb19846afb06801ec8df8fd83db6cca2aa3b
dimsp98 <dspiliotis98@gmail.com>
2025-02-08 13:55:18 +0200

    updated

--- config/bspwm/autostart.sh
+++ config/bspwm/autostart.sh
@@ -2,7 +2,7 @@
 
 #Xorg management
 #xrandr --output HDMI-2 --primary --mode 1920x1080 --rate 120 --dpi 60 --rotate normal --output eDP-1 --off &
-#xrandr --output eDP-1 --mode 1920x1080 --rate 60 --dpi 60 --rotate normal &
+xrandr --output eDP-1 --mode 1920x1080 --rate 60 --dpi 60 --rotate normal &
 
 #cursor
 xsetroot -cursor_name left_ptr &
